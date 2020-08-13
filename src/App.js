530bcc43737f06671c363a6df83caeac797c28f6
khaleelyounis <Khaleelyounis.dev@gmail.com>
2020-08-13 12:32:54 -0700

    project scaffolding + initial Table component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './components/Table';
+import data from './data/table.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { tableHeader, tableData } = data;
+
+  return <Table />;
 }
 
 export default App;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Table = () => {
+  return (
+    <table>
+      <thead></thead>
+      <tbody></tbody>
+    </table>
+  );
+};
+
+Table.defaultProps = {};
+
+Table.propTypes = {};
+
+export default Table;
